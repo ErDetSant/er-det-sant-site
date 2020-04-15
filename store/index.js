cf4a3d4d88cd0971dae6528c8bbfdcd890f78f1f
Jan Schjetne <schjetne@gmail.com>
2020-04-15 20:47:44 +0200

    Add authors to store

--- store/index.js
+++ store/index.js
@@ -4,6 +4,7 @@ Vue.use(Vuex)
 
 export const state = () => ({
   topics: [],
+  authors: [],
   articles: [],
   siteInfo: [],
 })
@@ -11,6 +12,9 @@ export const state = () => ({
 export const mutations = {
   SET_TOPICS(state, data) {
     state.topics = data
+  },
+  SET_AUTHORS(state, data) {
+    state.authors = data
   },
   SET_ARTICLES(state, data) {
     state.articles = data
@@ -23,9 +27,15 @@ export const mutations = {
 export const actions = {
   async nuxtServerInit({ commit }) {
     const topicsCtx = await require.context('~/content/topics/', false, /\.json$/)
+    const authorsCtx = await require.context('~/content/authors/', false, /\.json$/)
     const articlesCtx = await require.context('~/content/articles/', true, /\.json$/)
     const topics = topicsCtx.keys().map(key => {
       const res = topicsCtx(key)
+      res.slug = key.slice(2, -5)
+      return res
+    })
+    const authors = authorsCtx.keys().map(key => {
+      const res = authorsCtx(key)
       res.slug = key.slice(2, -5)
       return res
     })
@@ -37,6 +47,7 @@ export const actions = {
     const info = require('~/content/site/info.json')
 
     await commit('SET_TOPICS', topics)
+    await commit('SET_AUTHORS', authors)
     await commit('SET_ARTICLES', articles)
     await commit('SET_INFO', info)
   },
